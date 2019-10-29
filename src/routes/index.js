16f27173820add22816ca9ddee84290256b70c4c
Russkih Anatoly <titanofdarkness@gmail.com>
2019-10-29 19:13:19 +1000

    404

--- src/routes/index.js
+++ src/routes/index.js
@@ -180,4 +180,21 @@ module.exports = app => {
       res.status(500).json({ error: e });
     }
   });
+
+  app.use((req, res) => {
+    res.status(404);
+
+    // respond with html page
+    if (req.accepts('html')) {
+      return res.render('404', { url: req.url });
+    }
+
+    // respond with json
+    if (req.accepts('json')) {
+      return res.send({ error: 'Not found' });
+    }
+
+    // default to plain-text. send()
+    return res.type('txt').send('Not found');
+  });
 };
