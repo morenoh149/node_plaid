b3a09ef5c5dcb00731779b5eff714fe815cff187
Russkih Anatoly <titanofdarkness@gmail.com>
2019-10-30 19:12:22 +1000

    Minor

--- src/service/DbProvider.js
+++ src/service/DbProvider.js
@@ -3,8 +3,6 @@ const CryptoProvider = require('./CryptoProvider');
 
 class DbProvider {
   static async SaveToken(token, userId) {
-    const ts = CryptoProvider.encrypt(token);
-    const tv = CryptoProvider.decrypt(ts);
     const dbData = {
       token: CryptoProvider.encrypt(token),
       userId,
